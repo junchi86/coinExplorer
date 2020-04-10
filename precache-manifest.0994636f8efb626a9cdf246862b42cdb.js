b308072b3890a559c0c52ac7b4514031e03f7944
joon <56216292+junchi86@users.noreply.github.com>
2020-04-10 21:36:03 +0900

    build

---
+++ precache-manifest.0994636f8efb626a9cdf246862b42cdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34d6f03b2752b15a069e0bbec558ef5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5995fd53f5d917317406",
+    "url": "./static/js/2.1a00f5c2.chunk.js"
+  },
+  {
+    "revision": "f5a66d6c0f7e73552f3af9f6e9caf8d7",
+    "url": "./static/js/2.1a00f5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca87724f788edba45ff",
+    "url": "./static/js/main.4bf69364.chunk.js"
+  },
+  {
+    "revision": "7738267643b2559eeb5a",
+    "url": "./static/js/runtime-main.f06f1b34.js"
+  }
+]);
